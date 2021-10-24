c8afd9c3363e8eb90fadf2668382be6560d09d5f
pappa <pappannabella@yahoo.com>
2021-10-24 20:37:30 +0300

    Tema

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,29 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Rezolvare Tema
+
+        // 1.
+        System.out.println("Hello Annabella!");
+
+        // 2.
+        System.out.println(20 + 7);
+
+        // 3.
+        System.out.println((float) 55 / 9);
+
+        // 4.
+        // a.
+        System.out.println(-5 + 8 * 6);
+
+        // b.
+        System.out.println((55 + 9) % 9);
+
+        // c.
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(20 + (float) -3 * 5 / 8);
+        // d.
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
     }
 }
