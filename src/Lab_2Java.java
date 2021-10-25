34e5f129f0488f528b28726eb8db5c858d12fae3
pappa <pappannabella@yahoo.com>
2021-10-25 21:52:51 +0300

    Tema Lab 10 Exercitiu 1

--- src/Lab_2Java.java
+++ src/Lab_2Java.java
@@ -3,44 +3,33 @@ public class Lab_2Java {
     public static int z = 3;
 
     public static void main(String[] args) {
-        int result1 = sum(11,22);
-        int result2 = subs(x,z);
-        long result3 = mul(x,z);
-        float result4 = div(26,8);
-        float result5 = mod(x,z);
+        // Tema
+        // 1
+        Calculator calchere = new Calculator();
+        Print printhere = new Print();
 
-        System.out.println(result1+3);
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4+1);
-        System.out.println(result5);
+        printhere.PrintMyName();
+        printhere.PrintCosminsName();
+        System.out.println("\n");
 
-        System.out.println(sum(5,8));
-        System.out.println(subs(5,8));
-        System.out.println(mul(5,8));
-        System.out.println(div(5,8));
-        System.out.println(mod(10,3));
+        int result1 = calchere.sum(x, z);
+        int result2 = calchere.subs(x, z);
+        long result3 = calchere.mul(x, z);
+        float result4 = calchere.div(x, z);
+        float result5 = calchere.mod(x, z);
+
+        System.out.println("Rezultatul adunarii a " + x + " si " + z + " este: " +result1);
+        System.out.println("Rezultatul scaderii a " + x + " si " + z  + " este: " + result2);
+        System.out.println("Rezultatul inmultirii a " + x + " si " + z  + " este: " + result3);
+        System.out.println("Rezultatul impartirii a " + x + " la " + z  + " este: "+ result4);
+        System.out.println("Rezultatul modulo-ului a " + x + " cu " + z  + " este: " + result5 + "\n");
+        System.out.println(calchere.sum(5, 8));
+        System.out.println(calchere.subs(5, 8));
+        System.out.println(calchere.mul(5, 8));
+        System.out.println(calchere.div(5, 8));
+        System.out.println(calchere.mod(10, 3));
     }
-    public static int sum(int first, int second){
-        int result = first + second;
-        return result;
-    }
-    public static int subs(int first, int second){
-        int result = first - second;
-        return result;
-    }
-    public static long mul(long first, long second){
-        long result = first * second;
-        return result;
-    }
-    public static float div(float first, float second) {
-        float result = (first / second);
-        return  result;
-    }
-    public static float mod(float first, float second) {
-        float result = (first % second);
-        return result;
-    }
+
 }
 
 
