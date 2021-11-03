04a05bbcbf04969b1756ed770e3038ec9ef6952f
pappa <pappannabella@yahoo.com>
2021-11-03 17:25:38 +0200

    Tema Lab 11 if/else

---
+++ src/Ifelsemain/LogicalOp.java
@@ -0,0 +1,81 @@
+package Ifelsemain;
+
+public class LogicalOp {
+    public int checkBiggerNumber(int first, int second) {
+        if (first > second) {
+            return first;
+        }
+        return second;
+    }
+
+    public String exercise4(String text) {
+        if (text.equals("FastTrackIT")) {
+            return "Learning text comparison";
+        }
+        return "Got to try some more";
+    }
+
+    public String exercise5(String text, int number) {
+        if (text.equals("FastTrackIT") && number <= 3) {
+            return text + number;
+        } else if (!text.equals("FastTrackIT") && number >= 4) {
+            return number + text;
+        }
+        return "Keep trying!";
+    }
+
+    public String exercise6(int number) {
+        if (number > 8 || number == 6) {
+            return "The amount of snow this winter was(cm): " + number;
+        }
+        return "The forecast snow is(cm): " + number;
+    }
+
+    public String exercise7(int number) {
+        if (number > 3 && number != 4) {
+            return "The number is greater than 3 and not equal to 4";
+        } else if (number == 4) {
+            return "The number is equal to 4";
+        }
+        return "The number is lower than 3";
+    }
+
+    public void exercise8(int number) {
+        switch (number) {
+            case 1:
+                System.out.println("The number is: 1");
+                break;
+            case 2:
+                System.out.println("The number is: 2");
+                break;
+            case 3:
+                System.out.println("The number is: 3");
+                break;
+            case 4:
+                System.out.println("The number is: 4");
+                break;
+            case 5:
+                System.out.println("The number is: 5");
+                break;
+        }
+    }
+
+    public boolean isNumberEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public boolean isEligibleToVote(int age) {
+        return age > 18;
+    }
+
+    public int exercise10(int first, int second, int third) {
+        if (first >= second && first >= third) {
+            return first;
+        } else if (second >= first && second >= third) {
+            return second;
+        } else {
+            return third;
+        }
+    }
+}
+
---
+++ src/Ifelsemain/MainIfElse.java
@@ -0,0 +1,36 @@
+package Ifelsemain;
+
+import java.util.SortedMap;
+
+public class MainIfElse {
+    public static void main(String[] args) {
+        LogicalOp logicalop = new LogicalOp();
+        int biggest = logicalop.checkBiggerNumber(2, 8);
+        System.out.println("The bigger number is: " + biggest);
+        System.out.println(logicalop.checkBiggerNumber(28, 7));
+        System.out.println(logicalop.exercise4("FastTrack"));
+        System.out.println(logicalop.exercise4("FastTrackIT"));
+        System.out.println(logicalop.exercise5("FastTrackIT", 2));
+        System.out.println(logicalop.exercise5("FastTrackITT", 8));
+        System.out.println(logicalop.exercise5("FastTrackIT", 8));
+        System.out.println(logicalop.exercise5("FastTrackITT", 2));
+        System.out.println(logicalop.exercise6(6));
+        System.out.println(logicalop.exercise6(15));
+        System.out.println(logicalop.exercise6(7));
+        System.out.println(logicalop.exercise7(9));
+        System.out.println(logicalop.exercise7(4));
+        System.out.println(logicalop.exercise7(-5));
+        logicalop.exercise8(4);
+        logicalop.exercise8(15);
+        System.out.println(logicalop.isNumberEven(9));
+        System.out.println(logicalop.isNumberEven(12));
+        System.out.println(logicalop.isEligibleToVote(16));
+        System.out.println(logicalop.isEligibleToVote(19));
+        System.out.println(logicalop.exercise10(10,7,6));
+        System.out.println(logicalop.exercise10(10,10,10));
+        System.out.println(logicalop.exercise10(10,10,615));
+        System.out.println(logicalop.exercise10(10,20,6));
+        System.out.println(logicalop.isNumberEven(9));
+        System.out.println(logicalop.isNumberEven(12));
+    }
+}
